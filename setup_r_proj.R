4dcdb35f03f983a53c02f488354ecfcecf83997d
Fong Chun Chan <fongchunchan@gmail.com>
2017-12-14 22:55:52 +0000

    Add rprojroot to new R project

--- setup_r_proj.R
+++ setup_r_proj.R
@@ -6,3 +6,4 @@ Setup a new R project by installing nvimcom and colorout.
 
 devtools::install("~/.config/nvim/R/nvimcom")
 devtools::install_github("jalvesaq/colorout", force = TRUE)
+install.packages("rprojroot")
