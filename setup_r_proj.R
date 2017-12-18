2cc27439ce31cf50349f2c65992cfd1f9570ae68
f.chan <f.chan@achillestx.com>
2017-12-18 11:19:53 +0000

    Update setup_r_proj.R

--- setup_r_proj.R
+++ setup_r_proj.R
@@ -4,5 +4,6 @@ script_description <- "
 Setup a new R project by installing nvimcom and colorout.
 "
 
+install.packages("rprojroot")
 devtools::install("~/.config/nvim/R/nvimcom")
 devtools::install_github("jalvesaq/colorout", force = TRUE)
