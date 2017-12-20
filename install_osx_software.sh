b638eef1d13775881e8dcef8649839707f0e266c
f.chan <f.chan@achillestx.com>
2017-12-20 10:41:44 +0000

    Add ssh hostname completion

--- install_osx_software.sh
+++ install_osx_software.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 #
-# Install software for the OSX
+# Install software for the Mac OSX
+# This is called from the `install.sh` script
+#
 brew install \
 	wget \
 	neovim/neovim/neovim \
@@ -8,3 +10,9 @@ brew install \
 	htop \ # Fancy top command
 	rename \ # for renaming files
 	md5sha1sum # For md5sum command
+	grip # Preview README.md (or other md) using Github md (https://github.com/joeyespo/grip)
+
+# For ssh hostname completion
+# https://apple.stackexchange.com/a/209269
+brew install bash-completion
+brew tap homebrew/completions
