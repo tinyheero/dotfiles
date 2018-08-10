0af1d22389b3eb4ed62ff8a0ad4d3d0279124cba
Fong Chun Chan <f.chan@achillestx.com>
2018-08-10 14:17:49 +0000

    Add ack installation steps

--- install_linux_sofware.sh
+++ install_linux_sofware.sh
@@ -4,11 +4,20 @@
 # This is called from the `install.sh` script
 #
 
-#----------
+#
 # fzf
-#----------
+#
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install \
 	--key-bindings \
 	--completion \
 	--no-update-rc
+
+#
+# Install ack (programmer's grep)
+# Used by ack.vim if Ag (the_silver_searcher) is not available.
+#
+echo "Installing ack";
+curl https://beyondgrep.com/ack-2.24-single-file \
+    > "${HOME}/usr/bin/ack" \
+    && chmod 0755 "${HOME}/usr/bin/ack";
