7e07f461eaa67c512f1e20ce60d55b80ad6448d0
Fong Chun Chan <fongchunchan@gmail.com>
2018-10-15 09:10:47 +0000

    Update how fzf is installed

--- install_linux_sofware.sh
+++ install_linux_sofware.sh
@@ -7,17 +7,21 @@
 #
 # fzf
 #
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install \
-	--key-bindings \
-	--completion \
-	--no-update-rc
+#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+#~/.fzf/install \
+#	--key-bindings \
+#	--completion \
+#	--no-update-rc
 
 #
 # Install ack (programmer's grep)
 # Used by ack.vim if Ag (the_silver_searcher) is not available.
 #
+usr_bin_dir="${HOME}/usr/bin/";
+
+[[ -d "${HOME}/usr/bin/" ]] || mkdir -p "${usr_bin_dir}";
+
 echo "Installing ack";
 curl https://beyondgrep.com/ack-2.24-single-file \
-    > "${HOME}/usr/bin/ack" \
-    && chmod 0755 "${HOME}/usr/bin/ack";
+    > "${usr_bin_dir}/ack" \
+    && chmod 0755 "${usr_bin_dir}/ack";
