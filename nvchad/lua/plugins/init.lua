65b09bdd835ae57689a1876962d41fbd278e3a59
Fong Chun Chan <fongchunchan@gmail.com>
2024-08-28 14:51:45 -0700

    Add snakemake filetype support and R.nvim config

--- nvchad/lua/plugins/init.lua
+++ nvchad/lua/plugins/init.lua
@@ -51,6 +51,38 @@ return {
     lazy = false
   },
   {
+    "R-nvim/R.nvim",
+    config = function ()
+      -- Create a table with the options to be passed to setup()
+      local opts = {
+          hook = {
+            on_filetype = function()
+                vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
+                vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+            end
+          },
+          R_args = {"--quiet", "--no-save"},
+          min_editor_width = 72,
+          rconsole_width = 78,
+          disable_cmds = {
+              "RClearConsole",
+              "RCustomStart",
+              "RSPlot",
+              "RSaveClose",
+            },
+          }
+          -- Check if the environment variable "R_AUTO_START" exists.
+          -- If using fish shell, you could put in your config.fish:
+          -- alias r "R_AUTO_START=true nvim"
+          if vim.env.R_AUTO_START == "true" then
+              opts.auto_start = 1
+              opts.objbr_auto_start = true
+          end
+          require("r").setup(opts)
+      end,
+    lazy = false
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
     config = function ()
@@ -66,10 +98,6 @@ return {
       require("cmp").setup({ sources = {{ name = "cmp_r" }}})
       require("cmp_r").setup({ })
     end,
-  },
-  {
-    "R-nvim/R.nvim",
-    lazy = false
   },
   {
     "R-nvim/cmp-r",
