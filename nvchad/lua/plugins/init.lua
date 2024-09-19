3ac8daaae7df8a968311adce79aef8eb1ebccefb
Fong Chun Chan <fongchunchan@gmail.com>
2024-09-18 22:10:32 -0700

    Add GitHub Copilot

--- nvchad/lua/plugins/init.lua
+++ nvchad/lua/plugins/init.lua
@@ -141,5 +141,17 @@ return {
   },
   {
     "R-nvim/cmp-r",
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+       suggestion = {
+          auto_trigger = true,
+        }
+      })
+    end,
   }
 }
