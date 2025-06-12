5ff3b8bb7448007780e5f66e2a5f8004a6cb04f2
Fong Chun Chan <fongchunchan@gmail.com>
2025-06-12 11:45:14 -0700

    Add NvChad settings

---
+++ nvchad/lua/mappings.lua
@@ -0,0 +1,13 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+vim.g.mapleader = ","
+vim.keymap.set('n', '<LocalLeader>d', '<Plug>RDSendLine', { desc = 'Send to R the current line and move down to next line' })
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
