43de7d79988ebdcdd6086ef9d55b23fb7366eda0
Fong Chun Chan <fongchunchan@gmail.com>
2025-10-03 15:47:41 -0700

    Add nextflow tree-sitter config

--- nvchad/lua/configs/nvim-treesitter.lua
+++ nvchad/lua/configs/nvim-treesitter.lua
@@ -1,13 +1,33 @@
+-- Define the custom parser
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+-- Configure nextflow filetype
+parser_config.nextflow = {
+  install_info = {
+    url = "https://github.com/matthuska/tree-sitter-nextflow",
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+  filetype = "nextflow",
+}
+
+vim.filetype.add({
+  extension = {
+    nf = 'nextflow',
+  }
+})
+
 local options = {
   ensure_installed = {
     "bash",
+    "nextflow",
     "markdown",
     "markdown_inline",
     "python",
     "r",
     "rnoweb",
     "snakemake",
-    "yaml",
+    "yaml"
   },
   highlight = { enable = true },
 }
