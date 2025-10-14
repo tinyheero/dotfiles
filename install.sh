32f2a969c57af05e97bb6819653dd9d282e3eba9
Fong Chun Chan <fongchunchan@gmail.com>
2025-10-14 18:38:58 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ declare -A plugins=(
   [zsh-syntax-highlighting]="https://github.com/zsh-users/zsh-syntax-highlighting.git"
   [zsh-autosuggestions]="https://github.com/zsh-users/zsh-autosuggestions"
   [conda-zsh-completion]="https://github.com/conda-incubator/conda-zsh-completion.git"
+  [zsh-bat]="https://github.com/fdellwing/zsh-bat.git"
 )
 
 for plugin in "${!plugins[@]}"; do
